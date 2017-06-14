c7673f00c1328bda00e4111d10978c0245227a2c
mugemeydan <mugeozc@yahoo.com>
2017-06-14 10:27:55 -0400

    created notitle-pageworked on template-parts , created content-page

--- wp-content/themes/childoftwenteysixteen/page.php
+++ wp-content/themes/childoftwenteysixteen/page.php
@@ -1,33 +1,34 @@
 <?php
 /**
- * Template Name:no-sidebar.php
+ * The template for displaying pages
  *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
  *
- * This is the template that displays all pages without asidebar
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
  */
 
 if (is_front_page() ):
 	get_header( 'home');
-else:
+else
 	get_header();
-endif; ?>
+endif; 
+?>
 
- 
+
+
+
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 		<?php
 		// Start the loop.
 		while ( have_posts() ) : the_post();
 
-			// Include the page content template. If it's the home page load a special template without a title
-			if (is_front_page () ):
-
-			get_template_part ('notitle', 'page' );
-
-				else:
+			// Include the page content template.
 			get_template_part( 'template-parts/content', 'page' );
-
-		endif; 
 
 			// If comments are open or we have at least one comment, load up the comment template.
 			if ( comments_open() || get_comments_number() ) {
@@ -44,5 +45,5 @@ endif; ?>
 
 </div><!-- .content-area -->
 
-<?php// get_sidebar(); ?>
+<?php //get_sidebar(); ?>
 <?php get_footer(); ?>
